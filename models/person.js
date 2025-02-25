cf8cc029b3b6ee46b4a971072fff11601193f026
devika <devikasanthosh593@gmail.com>
2025-02-25 09:20:24 +0530

    Deploying 3.22

--- models/person.js
+++ models/person.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
--- mongo.js
+++ mongo.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 require('dotenv').config();
 const mongoose = require('mongoose');
 
